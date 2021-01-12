b598f2fabaf8b6a72dc7a190a5ca1c51469a02ab
RJ97600 <46113438+RJ97600@users.noreply.github.com>
2021-01-12 23:24:54 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+from time import *
+from subprocess import *
+from hashlib import *
+import marshal
+import getpass
+import json
+import random
+import os
+
+try:
+    import requests as r
+except ImportError:
+    os.system("pip2 install requests")
+
+
+def main():
+    os.system('clear')
+    R = '\033[31;1m'
+
+    print(R+'###########################################\n')
+    print(R+'#-- Created By RJ97600 for FB Hack Prank --#\n\n')
+    print(R+'###########################################\n')
+
+
+    uid = raw_input('Username : ')
+    pwd = raw_input('Password : ')
+
+
+
+main()
